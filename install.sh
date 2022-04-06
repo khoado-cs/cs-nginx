9336f204a19604a30a9692d74ae0dc50635f3691
khoa.do <khoa.do@codestringers.com>
2022-04-06 16:01:29 +0700

    add lua module

--- install.sh
+++ install.sh
@@ -1,35 +1,40 @@
-#!/bin/bash
-id -u
+#!/bin/sh
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Not running as root"
-    exit
-fi
-
-# export PAR_PATH=$(pwd)
-export PAR_PATH="/etc/rapidcode"
+export PAR_PATH=$(pwd)
+export INSTALL_PATH="/etc/rapidcode"
 export PATH_SERVICE="/etc/systemd/system"
-export PREFIX=$PAR_PATH/nginx
+export PREFIX=$INSTALL_PATH/nginx
 export USER_RC="www-rc"
-# while getopts prefix: flag
-# do
-#     case "${flag}" in
-#         prefix) PREFIX=${OPTARG}/nginx;; 
-#     esac
-# done
+export LUAPAT="$PREFIX/luajit"
+#build luajit
+cd $PAR_PATH/dependent/luajit2-2.1
+make && sudo make install PREFIX=$LUAPAT
+
+export LUAJIT_LIB=$LUAPAT/lib
+export LUAJIT_INC=$LUAPAT/include/luajit-2.1
+#end build luajit
+
+cd $PAR_PATH/dependent/lua-resty-core
+make install PREFIX=$LUAPAT
+cd $PAR_PATH/dependent/lua-resty-lrucache
+make install PREFIX=$LUAPAT
+
 useradd --system --no-create-home --shell=/sbin/nologin $USER_RC
 
-cd bin
+cd $PAR_PATH/bin
 ./configure --prefix=$PREFIX --user=$USER_RC --with-http_ssl_module --with-http_v2_module --with-threads \
 --with-file-aio --with-http_auth_request_module --with-mail --with-mail_ssl_module --with-stream \
---with-stream_ssl_module
+--with-stream_ssl_module --add-module=$PAR_PATH/modules/lua-nginx-module \
+--add-module=$PAR_PATH/dependent/ngx_devel_kit --with-ld-opt="-Wl,-rpath,$LUAPAT"
 
 make
 
 make install
 
 make clean
-cat > $PAR_PATH/nginx.service <<- EOM
+
+#creaet service for linux
+cat > $INSTALL_PATH/nginx.service <<- EOM
 [Unit]
 Description=nginx rapidcode - high performance web server
 Documentation=http://nginx.org/en/docs/
@@ -40,7 +45,7 @@ Wants=network-online.target
 Type=forking
 PIDFile=$PREFIX/logs/nginx.pid
 ExecStartPre=$PREFIX/sbin/nginx -t
-ExecStart=$PREFIX/sbin/nginx -c $PREFIX/conf/nginx.conf
+ExecStart=$PREFIX/sbin/nginx -c $PREFIX/conf/nginx.conf -p $PREFIX
 ExecReload=$PREFIX/sbin/nginx -s reload
 ExecStop=$PREFIX/sbin/nginx -s QUIT $MAINPID
 PrivateTmp=true
@@ -59,9 +64,9 @@ sudo chown -R $USER_RC $PREFIX
 sudo chown -R $USER_RC /var/www
 
 sudo chmod -R 777 $PREFIX
-sudo chmod -R 777 $PAR_PATH/nginx.service
+sudo chmod -R 777 $INSTALL_PATH/nginx.service
 
-cat $PAR_PATH/nginx.service > $PATH_SERVICE/ngx-rapidcode.service
+cat $INSTALL_PATH/nginx.service > $PATH_SERVICE/ngx-rapidcode.service
 
 sudo systemctl daemon-reload 
 
